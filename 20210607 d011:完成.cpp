28747fe8648d2a107c2bfb29c75bea0e1765aeb8
Leo <75311991+wendy062644@users.noreply.github.com>
2021-06-07 14:00:37 +0800

    Create 20210607 d011:完成.cpp

---
+++ 20210607 d011:完成.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+	long long int a, b, ma = 0, num = 0;
+	cin >> a >> b;
+	for(a; b >= a; a++)
+	{
+		long long int m = a, n = a;
+		while(1)
+		{
+			if(m%2==0){
+				m = m / 2;
+				n = n + m;
+			}
+			else{
+				m = m * 3 + 1;
+				n = n + m;
+			}
+			if(m == 1){break;}
+		}
+		if(n > ma){ma = n; num = a;}
+	}
+	cout << ma << " " << num <<endl;
+}
+
+題目: http://www.tcgs.tc.edu.tw:1218/ShowProblem?problemid=d011
+d011: 98年台中區第一題(98年台中區複賽)
